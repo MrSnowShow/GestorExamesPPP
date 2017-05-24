ecbfca81183f07f96cb84869c8227da45c47a92b
Roman <next-r@hotmail.com>
2017-05-24 18:12:05 +0100

    remove spaces

--- funcoes.h
+++ funcoes.h
@@ -2,11 +2,9 @@
 #define FUNCOES_H_INCLUDED
 
 /* Funcoes auxiliares */
-
 void remove_barraN(char *minha_string);
 
 /* Funcoes de print das estruturas */
-
 void print_data(Data d);
 void print_hora(Hora h);
 void print_disciplina(Disciplina *d);
@@ -14,13 +12,11 @@ void print_aluno(Aluno *a);
 void print_exame(Exame *e);
 
 /* Funcoes para dar print as listas ligadas */
-
 void print_listaDisciplinas(Node_disciplina *listaD);
 void print_listaAlunos(Node_aluno *listaA);
 void print_listaExames(Node_exame *listaE);
 
-/* Funcoes para criar as estruturas */
-
+/* Funcoes para criar as estruturas apartir do utilizador */
 Data cria_data();
 Hora cria_hora();
 Disciplina* cria_disciplina();
@@ -28,19 +24,16 @@ Aluno* cria_aluno();
 Exame* cria_exame();
 
 /* Funcoes para inicializar (fazer mallocs) as estruturas de pointeiros */
-
 Disciplina* init_disciplina();
 Aluno* init_aluno();
 Exame* init_exame();
 
 /* Funcoes para inicializar (fazer malloc e por a NULL) as listas ligadas */
-
 Node_disciplina* init_nodeDisciplina();
 Node_aluno* init_nodeAluno();
 Node_exame* init_nodeExame();
 
 /* Funcoes para inserir nas listas ligadas */
-
 void inserir_listaDisciplinas(Node_disciplina *listaD, Disciplina *novaD);
 void inserir_listaAlunos(Node_aluno *listaA, Aluno *novoA);
 void inserir_listaExames(Node_exame *listaE, Exame *novoE);
@@ -49,7 +42,6 @@ void inserir_listaExames(Node_exame *listaE, Exame *novoE);
 void remover_listaDisciplinas(Node_disciplina *listaD, char *nome);
 
 /* Funcoes para ler dos ficheiros */
-
 Node_disciplina* ler_disciplinas();
 
 #endif /* FUNCOES_H_INCLUDED */
